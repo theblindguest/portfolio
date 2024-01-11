327146f6e94e2c69ce36f6978a62dfb81a9522e4
Jack Watson <jackwatson@Jacks-MacBook-Pro.local>
2024-01-11 22:17:30 +0000

    making about home

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Home />
+        element: <About />
       },
       // other pages....
       {
